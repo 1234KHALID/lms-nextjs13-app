c64adcb3c6afde6e1aa8e2f86c191deea16ae368
Mohammad Khalid <mohammad@prodios.com>
2023-11-18 11:13:30 +0530

    create post api for course

---
+++ app/api/courses/route.ts
@@ -0,0 +1,25 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const { title } = await req.json();
+
+    if (!userId) {
+      return new NextResponse("Unautherized", { status: 401 });
+    }
+
+    const course = await db.course.create({
+      data: {
+        userId,
+        title,
+      },
+    });
+
+    return NextResponse.json(course);
+  } catch (error) {
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
