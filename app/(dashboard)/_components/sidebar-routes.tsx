131b49c8cf17c2c2368ca615363ac8007bc7cf48
Mohammad Khalid <mohammad@prodios.com>
2023-11-18 11:02:58 +0530

    add sidebar navbar and mobile-sidebar components

---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,51 @@
+"use client";
+import React from "react";
+import SidebarItem from "./sidebar-item";
+import { BarChart, Compass, Layout, List } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+const guestRoutes = [
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: Compass,
+    label: "Browser",
+    href: "/search",
+  },
+];
+
+const teacherRoutes = [
+  {
+    icon: List,
+    label: "Courses",
+    href: "/teacher/courses",
+  },
+  {
+    icon: BarChart,
+    label: "Analytics",
+    href: "/teacher/analytics",
+  },
+];
+
+const SidebarRoutes = () => {
+  const pathName = usePathname();
+  const isTeacherPage = pathName?.includes("/teacher");
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
+  return (
+    <div className="flex flex-col w-full">
+      {routes.map((route) => (
+        <SidebarItem
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SidebarRoutes;
