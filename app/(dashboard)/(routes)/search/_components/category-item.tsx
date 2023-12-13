9dd0db5474b06aa220858a8bbfc38817bb189a14
Mohammad Khalid <mohammad@prodios.com>
2023-12-13 12:45:35 +0530

    Add data table and course card and course progress

--- app/(dashboard)/(routes)/search/_components/category-item.tsx
+++ app/(dashboard)/(routes)/search/_components/category-item.tsx
@@ -1,19 +1,4 @@
-import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
-
-import { db } from "@/lib/db";
-import { SearchInput } from "@/components/search-input";
-import { getCourses } from "@/app/actions/get-courses";
-import { CoursesList } from "@/components/courses-list";
-
-import { Categories } from "./_components/categories";
-
-interface SearchPageProps {
-  searchParams: {
-    title: string;
-    categoryId: string;
-  }
-}; "use client";
+"use client";
 
 import qs from "query-string";
 import { IconType } from "react-icons";
@@ -72,40 +57,3 @@ export const CategoryItem = ({
     </button>
   )
 }
-
-const SearchPage = async ({
-  searchParams
-}: SearchPageProps) => {
-  const { userId } = auth();
-
-  if (!userId) {
-    return redirect("/");
-  }
-
-  const categories = await db.category.findMany({
-    orderBy: {
-      name: "asc"
-    }
-  });
-
-  const courses = await getCourses({
-    userId,
-    ...searchParams,
-  });
-
-  return (
-    <>
-      <div className="px-6 pt-6 md:hidden md:mb-0 block">
-        <SearchInput />
-      </div>
-      <div className="p-6 space-y-4">
-        <Categories
-          items={categories}
-        />
-        <CoursesList items={courses} />
-      </div>
-    </>
-  );
-}
-
-export default SearchPage;
